102a56a1451656e321367c9e19e453e0f0441fef
stingermeta <andycanada420@gmail.com>
2026-05-23 02:14:39 +0800

    Update ao-minimum-resell.py
    - Minor adjustments

--- ao-minimum-resell.py
+++ ao-minimum-resell.py
@@ -6,7 +6,7 @@ profit_tax = 0.1
 total_tax = listing_tax + profit_tax
 
 # Replace the number with how much you bought an item for
-drachma_spent = None
+drachma_spent = 10000
 
 # Return the minimum price you should sell at to make a profit of at least 1 drachma
 def min_resell_price(spent):
@@ -16,11 +16,11 @@ def min_resell_price(spent):
         if(spent <= max_price and spent > 0):
             return (math.ceil((spent + 1) / (1 - 1 * total_tax)))
         elif(spent > max_price):
-            print (rule_text + ', provided value (' + str(spent) + ') exceeds the limit of ' + str(max_price) + '!')
+            print (rule_text + '; provided value (' + str(spent) + ') exceeds the limit of ' + str(max_price) + '!')
         elif(spent < 1):
-            print (rule_text + ', provided value ' + str(spent) + ') is less than 1!')
+            print (rule_text + '; provided value ' + str(spent) + ') is less than 1!')
     else:
-        print (rule_text + ', provided value (' + str(spent) + ') is not an integer!')
+        print (rule_text + '; provided value (' + str(spent) + ') is not an integer!')
 
 result = (min_resell_price(drachma_spent))
 
